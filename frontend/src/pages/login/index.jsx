3e43d851f100f2d9cc5984bdad88ecf1c1cd910e
Josef1923 <schererjos01@gmail.com>
2025-02-26 18:36:19 +0100

    Ajout body logIn

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -1,10 +1,12 @@
 import Header from "@comp/layout/header";
 import Footer from "@comp/layout/footer";
+import SignIn from "@comp/page/login/signin";
 
 function login() {
   return (
     <>
       <Header />
+        <SignIn />
       <Footer />
     </>
   );
