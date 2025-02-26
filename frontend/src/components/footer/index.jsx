5a70c891de7532f56764960172d3ae195d163aea
Josef1923 <schererjos01@gmail.com>
2025-02-26 13:30:54 +0100

    Ajout footer component

---
+++ frontend/src/components/footer/index.jsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+function Footer() {
+    return (
+      <footer className="footer">
+        <p className="footer-text">Copyright 2024 Argent Bank</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -1,9 +1,11 @@
 import Header from "../../components/header";
+import Footer from "../../components/footer";
 
 function login() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
