f8b53de1a01f29ac80efbe23351bb1217452a2c9
Josef1923 <schererjos01@gmail.com>
2025-03-09 15:36:36 +0100

    Ajout visuel de modification nom et prénom

--- frontend/src/components/page/dashboard/welcome/index.jsx
+++ frontend/src/components/page/dashboard/welcome/index.jsx
@@ -1,14 +1,41 @@
 import { useSelector } from "react-redux";
+import { useState } from "react";
 import "./styles.css";
 
 function WelcomeMessage() {
 
   const user = useSelector((state) => state.user.user);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEdit = () => {
+    setIsEditing(!isEditing);
+  };
 
   return (
     <div className="header">
-      <h1>Welcome back<br/>{user.firstName} {user.lastName}!</h1>
-      <button className="edit-button">Edit Name</button>
+      {isEditing ? (
+        <>
+          <h1>Welcome back</h1>
+          <div className="inputs">
+            <input
+              type="text"
+              placeholder= {user.firstName}
+            />
+            <input
+              type="text"
+              placeholder= {user.lastName}
+            />
+          </div>
+          <div className="editButtons">
+            <button className="edit-button" onClick={() => setIsEditing(false)}>Save</button>
+            <button className="edit-button" onClick={() => setIsEditing(false)}>Cancel</button>
+          </div>
+        </>) :
+        (<>
+          <h1>Welcome back<br />{user.firstName} {user.lastName}!</h1>
+          <button className="edit-button" onClick={handleEdit}>Edit Name</button>
+        </>)
+      }
     </div>
   );
 }
